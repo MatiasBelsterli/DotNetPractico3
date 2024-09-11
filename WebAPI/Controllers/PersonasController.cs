6eddac033b747b117a2256a7a1e7b23041990522
Matias Belsterli De Vera <129549029+MatiasBelsterli@users.noreply.github.com>
2024-09-11 17:29:39 -0300

    add personas & vehiculos controller

---
+++ WebAPI/Controllers/PersonasController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.IBLs;
+using Shared;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase {
+        private readonly IBL_Personas _bl;
+
+        public PersonasController(IBL_Personas bl) {
+            _bl = bl;
+        }
+
+        // GET: api/personas
+        [HttpGet]
+        public List<Persona> Get() {
+            return _bl.Get();
+        }
+
+        // GET api/personas/5
+        [HttpGet("{documento}")]
+        public Persona Get(string documento, int test) {
+            
+            return _bl.Get(documento);
+        }
+
+        // POST api/personas
+        [HttpPost]
+        public void Post([FromBody] Persona persona) {
+            _bl.Insert(persona);
+        }
+
+        // PUT api/personas/5
+        [HttpPut("{id}")]
+        public void Put([FromBody] Persona persona) {
+            _bl.Update(persona);
+        }
+
+        // DELETE api/personas/5
+        [HttpDelete("{id}")]
+        public void Delete(string documento) {
+            _bl.Delete(documento);
+        }
+    }
+}
---
+++ WebAPI/Controllers/VehiculosController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.IBLs;
+using Shared;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculosController : ControllerBase {
+        private readonly IBL_Vehiculos _bl;
+
+        public VehiculosController(IBL_Vehiculos bl) {
+            _bl = bl;
+        }
+
+        // GET: api/vehiculos
+        [HttpGet]
+        public List<Vehiculo> Get() {
+            return _bl.Get();
+        }
+
+        // GET api/vehiculos/5543
+        [HttpGet("{documento}")]
+        public Vehiculo Get(string matricula) {
+            
+            return _bl.Get(matricula);
+        }
+
+        // POST api/vehiculos
+        [HttpPost]
+        public void Post([FromBody] Vehiculo vehiculo) {
+            _bl.Insert(vehiculo);
+        }
+
+        // PUT api/vehiculos/5564
+        [HttpPut("{id}")]
+        public void Put([FromBody] Vehiculo vehiculo) {
+            _bl.Update(vehiculo);
+        }
+
+        // DELETE api/vehiculos/5453
+        [HttpDelete("{id}")]
+        public void Delete(string matricula) {
+            _bl.Delete(matricula);
+        }
+    }
+}
